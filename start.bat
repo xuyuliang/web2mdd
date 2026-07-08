1bc0c1c77371dfece9f07f79d951b32c0c38fe66
Xu Yuliang <xuyuliang@gmail.com>
2026-07-08 08:01:46 +0800

    0708

--- start.bat
+++ start.bat
@@ -21,5 +21,4 @@ echo.
 echo 启动服务器（按 Ctrl+C 停止）...
 echo.
 python -m uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
-
 pause
--- start2.bat
+++ start2.bat
@@ -3,6 +3,6 @@
 echo.
 echo 启动服务器（按 Ctrl+C 停止）...
 echo.
-python -m uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
-
+echo python -m uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+python -m uvicorn app.main:app --host 0.0.0.0 --port 8000
 pause
